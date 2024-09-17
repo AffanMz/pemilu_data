de7b465b178ce414e20a69f8b93635720ba2e5f3
AffanMz <affansukses99@gmail.com>
2024-09-17 09:59:35 +0700

    finised feature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'admindata',
             'email' => 'test@example.com',
         ]);
     }
